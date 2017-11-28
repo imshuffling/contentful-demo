34d758559466d06e50c613d8564e12279e06288c
David Riches <hi@davidrich.es>
2017-11-28 09:12:07 +0000

    making some updates

--- src/article/ArticleListItem.js
+++ src/article/ArticleListItem.js
@@ -6,14 +6,24 @@ class ArticleListItem extends React.Component {
 
         const {article} = this.props
 
+        console.log({article})
+
         return (
             <article className='post'>
-                <h2>{article.fields.title}</h2>
+                <div className="article-header" style={{backgroundImage: 'url(' + article.fields.featureImage.fields.file.url + '?w=1200' +')'}}>
+                    <h2>{article.fields.title}</h2>
+                    <p className="date">{article.fields.publishDate}</p>
+                </div>
+                <div className="author-info">
+                    <div className="author-info-image">
+                        <img alt={article.fields.author.fields.avatar.fields.title} src={article.fields.author.fields.avatar.fields.file.url + '?w=100&h=100'} />
+                    </div>
+                    <div className="author-info-text">
+                        <h3>{article.fields.author.fields.fullName}</h3>
+                        <p>{article.fields.author.fields.bio}</p>
+                    </div>
+                </div>
                 <p>{article.fields.content}</p>
-
-                <p>{article.fields.author.fields.fullName}</p>
-                <img alt="" src={article.fields.author.fields.avatar.fields.file.url} />
-
             </article>
         )
     }
